16ab15a434306e9a4ae4ef4d3dce4e4eac55a122
anatoliy <anatoliy.safonov.ua@gmail.com>
2023-08-31 16:34:04 +0300

    Update user.py
    add user model

---
+++ PhotoShare/app/models/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, declarative_base
+from PhotoShare.app.core.database import engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(250), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    # avatar: Mapped[str] = mapped_column(String(255), nullable=True)
+    refresh_token: Mapped[str] = mapped_column(String(255), nullable=True)
+    confirmed: Mapped[bool] = mapped_column(Boolean, default=False)
+
+
+Base.metadata.create_all(bind=engine)
