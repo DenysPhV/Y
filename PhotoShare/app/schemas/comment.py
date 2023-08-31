3e00e4e3f694173698d0e35635760729782f5321
OOlexandr <oleksarkhumua@gmail.com>
2023-08-31 18:44:48 +0300

    Added models for comments and basic functionality

---
+++ PhotoShare/app/schemas/comment.py
@@ -0,0 +1,16 @@
+from typing import List
+from pydantic import BaseModel, Field
+from datetime import datetime
+from PhotoShare.app.schemas import photo as p
+from PhotoShare.app.schemas import user as u
+
+class CommentModel(BaseModel):
+    content: str = Field(max_length=265)
+
+class CommentResponse(CommentModel):
+    id: int
+    created_at: datetime
+    updated_at: List[datetime]
+    
+    class Config:
+        orm_mode = True
